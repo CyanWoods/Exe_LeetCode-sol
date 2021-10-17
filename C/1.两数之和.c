e87c13884efa199aafcb0dc2badf7aa852dc886a
cyanwoods <wyz597491644@gmail.com>
2021-10-17 10:20:28 +0800

    .

--- C/1.两数之和.c
+++ C/1.两数之和.c
@@ -5,14 +5,29 @@
  * [1] 两数之和
  */
 
+
+
 // @lc code=start
-
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-ddd
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    int* value=malloc(2*sizeof(int));        
+    for (int i=0; i<numsSize-1; i++)
+    {
+        for (int j=i+1; j<numsSize; j++)
+        {
+            if (nums[i]+nums[j]==target)
+            {
+                value[0]=i;
+                value[1]=j;
+                break;
+            }
+        }
+    }
+                *returnSize=2;
+                return value;
 }
 // @lc code=end
 
---
+++ C/1170.比较字符串最小字母出现频次.c
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1170 lang=c
+ *
+ * [1170] 比较字符串最小字母出现频次
+ */
+
+// @lc code=start
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* numSmallerByFrequency(char ** queries, int queriesSize, char ** words, int wordsSize, int* returnSize){
+
+}
+// @lc code=end
+
